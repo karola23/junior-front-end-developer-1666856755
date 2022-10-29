10f6fe07755f91d387ddcf4b091a7c3907cfb6d8
Karolina Raźniak <90191196+karola23@users.noreply.github.com>
2022-10-30 01:57:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+export default App;
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+    render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+   );
+  }
 }
-
 export default App;
